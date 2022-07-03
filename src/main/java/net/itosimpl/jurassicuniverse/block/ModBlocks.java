7d37ba628b3743768a3e4754ec7dd23f1b91c368
ItoSimple <itouattara907@gmail.com>
2022-07-03 04:43:35 +0000

    added a comment

--- src/main/java/net/itosimpl/jurassicuniverse/block/ModBlocks.java
+++ src/main/java/net/itosimpl/jurassicuniverse/block/ModBlocks.java
@@ -4,6 +4,7 @@ import net.itosimpl.jurassicuniverse.JurassicUniverse;
 import net.itosimpl.jurassicuniverse.item.ModCreativeModeTab;
 import net.itosimpl.jurassicuniverse.item.ModItems;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -18,26 +19,26 @@ import java.util.function.Supplier;
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, JurassicUniverse.MOD_ID);
-
+    
     public static final RegistryObject<Block> AMBER_ORE = registerBlock("amber_ore",
             () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(5f).requiresCorrectToolForDrops()));
+                    .strength(5f).requiresCorrectToolForDrops()), ModCreativeModeTab.JURASSI_BLOCKS);
 
     public static final RegistryObject<Block> DEEPSLATE_AMBER_ORE = registerBlock("deepslate_amber_ore",
             () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(7f).requiresCorrectToolForDrops()));
+                    .strength(7f).requiresCorrectToolForDrops()), ModCreativeModeTab.JURASSI_BLOCKS);
 
 
-
-    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
-        registerBlockItem(name, toReturn);
+        registerBlockItem(name, toReturn, tab);
         return toReturn;
     }
 
-    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
+                                                                            CreativeModeTab tab) {
         return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
-                new Item.Properties().tab(ModCreativeModeTab.JURASSI_BLOCKS)));
+                new Item.Properties().tab(tab)));
     }
 
     public static void register(IEventBus eventBus) {
